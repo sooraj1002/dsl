d801961d1b41d20fd504c55eaff472e75af18052
sooraj1002 <ksooraj162@gmail.com>
2024-07-04 20:45:30 +0530

    added dynamo-prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,109 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, Logger } from '@nestjs/common';
+import {
+  Schema,
+  applyMigrations,
+  createMigrations,
+  generatePrismaSchemaFile,
+} from '@techsavvyash/dynamo-prisma';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger: Logger = new Logger(AppService.name);
+  async getHello() {
+    let data = {
+      schema: [
+        {
+          schemaName: 'cooooooooooolz',
+          fields: [
+            {
+              fieldName: 'id',
+              type: 'String',
+              description: '',
+              maxLength: '',
+              isNullable: false,
+              isUnique: true,
+              isVectorEmbed: false,
+            },
+            {
+              fieldName: 'question',
+              type: 'String',
+              description: '',
+              maxLength: '',
+              isNullable: false,
+              isUnique: false,
+              isVectorEmbed: true,
+              embeddingAlgo: 'text-embedding-ada-002',
+            },
+            {
+              fieldName: 'answer',
+              type: 'String',
+              description: '',
+              maxLength: '',
+              isNullable: false,
+              isUnique: false,
+              isVectorEmbed: true,
+              embeddingAlgo: 'text-embedding-ada-002',
+            },
+          ],
+          description: 'schema for questions and answers',
+        },
+        {
+          schemaName: 'questions01',
+          fields: [
+            {
+              fieldName: 'question',
+              type: 'String',
+              description: '',
+              maxLength: 10,
+              isNullable: false,
+              isUnique: true,
+              isVectorEmbed: false,
+              isUuid: false,
+              embeddingAlgo: 'text-embedding-ada-002',
+            },
+            {
+              fieldName: 'question007',
+              type: 'String',
+              description: '',
+              maxLength: 10,
+              isNullable: true,
+              isUnique: true,
+              isVectorEmbed: false,
+              isUuid: false,
+              embeddingAlgo: 'text-embedding-ada-002',
+            },
+            {
+              fieldName: 'answer',
+              type: 'String',
+              description: '',
+              maxLength: '',
+              isNullable: false,
+              isId: true,
+              isUuid: false,
+              isAutoIncrement: false,
+              isUnique: false,
+              isVectorEmbed: false,
+              // embeddingAlgo: 'text-embedding-ada-002',
+            },
+          ],
+          description: 'schema for questions and answers',
+        },
+      ],
+    };
+
+    try {
+      let schema = await generatePrismaSchemaFile(
+        data as any as Schema,
+        'prisma/schema.prisma',
+      );
+      await createMigrations(schema);
+      await applyMigrations();
+      this.logger.log('this is the generated schema from prisma dsl');
+      this.logger.log(schema);
+    } catch (e) {
+      this.logger.error('found an error');
+      this.logger.error(e);
+      throw new ConflictException(`Error creating schema ${e}`);
+    }
   }
 }
